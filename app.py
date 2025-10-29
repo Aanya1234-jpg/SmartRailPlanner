050760145b34ee92e77290a6374feb0567c97611
Aanya1234-jpg <aany13077@gmail.com>
2025-10-29 15:11:12 +0530

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import joblib
 from datetime import datetime, timedelta
 
 # ---------------------- STYLING ----------------------
-st.set_page_config(page_title="SmartRail Planner", page_icon="🚆", layout="left-top")
+st.set_page_config(page_title="SmartRail Planner", page_icon="🚆", layout="left")
 
 st.markdown("""
 <style>
@@ -142,5 +142,6 @@ st.markdown(
     "<div style='text-align:center; color:white;'>© 2025 SmartRail Planner | Designed by Aanya Sinha</div>",
     unsafe_allow_html=True
 )
+
 
 
